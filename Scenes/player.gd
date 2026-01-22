5e1c9f228e07d006b949092b56c1a154d632808e
jatgm <jatgmalt@protonmail.com>
2026-01-22 13:34:14 -0800

    E(?)

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -320.0
 
 const ACCELERATION = 800.0
 const FRICTION = 1000.0
--- Scripts/camera_2d.gd
+++ Scripts/camera_2d.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if $"../Player".position.x > position.x:
+	if $"../Player".position.x > position.x and $"../Player".position.x >= 70:
 		position.x = round($"../Player".position.x)
